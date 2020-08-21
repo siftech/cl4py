9ebbf8e805909ea2cb02550630de51edbfe6baab
Robert P. Goldman <rpgoldman@goldman-tribe.org>
2020-08-21 10:18:18 -0500

    Test to try to identify problem.
    Problem is that at some point in JSON handling a boolean value of True is transformed to the string "true". Have not been able to identify where this happens.

---
+++ test/sample-json-output.lisp
@@ -0,0 +1,6 @@
+(in-package :common-lisp-user)
+
+(defparameter status
+  "{ \"flag\": true}")
+
+(format t "~&Status is ~s~%" status)
--- test/test_for_readtable.py
+++ test/test_for_readtable.py
@@ -1,6 +1,8 @@
+import os
+import json
 from pytest import fixture
 import cl4py
-import os
+
 
 # pytest forces violation of this pylint rule
 # pylint: disable=redefined-outer-name
@@ -16,7 +18,9 @@ def cl(lisp):
     return lisp.function("find-package")("CL")
 
 
-# This test verifies issue underlying MR #9
+# This test verifies issue underlying MR #9, where
+# the readtable variable of the lisp process was bound with
+# too wide a scope, breaking reading of arbitrary things.
 def test_readtable_problem(cl):
     retval = cl.compile_file(
         os.path.join(os.path.dirname(__file__), "sample-program.lisp")
@@ -37,3 +41,15 @@ def cleanup(outfile):
             os.remove(outfile)
         except:                 # pylint: disable=bare-except
             pass
+
+
+def test_json_boolean_problem(cl):
+    retval = cl.load(
+        os.path.join(os.path.dirname(__file__), "sample-json-output.lisp")
+    )
+    assert retval
+    # foo = cl_user.status
+    json_string = cl.eval(cl4py.Symbol('STATUS', "COMMON-LISP-USER"))
+    print(json_string)
+    js = json.loads(json_string)
+    assert js['flag'] == True # pylint: disable=singleton-comparison
